f60467e0776d0f29124f8d1577b4d59d20b54a5e
nexus122 <juan-pablo122@hotmail.es>
2024-11-06 20:51:54 +0100

    [Refactor-del-codigo] fix: Solve type problems

--- src/app/services/todolist.service.ts
+++ src/app/services/todolist.service.ts
@@ -8,26 +8,29 @@ export class TodolistService {
   constructor(private local: LocalStorageService) {}
   loadInitialTodos(): void {
     if (typeof window !== 'undefined') {
-      const todos = this.local.getFromLocalStorage(this.local.LOCALKEY);
+      const todos: Todo[] =
+        this.local.getFromLocalStorage(this.local.LOCALKEY) || [];
       this.local.updateObservable(todos);
     }
   }
 
   addTodo(todo: Todo): void {
-    const todos = this.local.getFromLocalStorage(this.local.LOCALKEY) || [];
+    const todos: Todo[] =
+      this.local.getFromLocalStorage(this.local.LOCALKEY) || [];
     todos.push(todo);
     this.local.updateObservable(todos);
   }
 
   removeTodo(id: number): void {
-    const todos = this.local
-      .getFromLocalStorage(this.local.LOCALKEY)
-      .filter((todo: Todo) => todo.id !== id);
-    this.local.updateObservable(todos);
+    const todos: Todo[] =
+      this.local.getFromLocalStorage(this.local.LOCALKEY) || [];
+    const updatedTodos = todos.filter((todo: Todo) => todo.id !== id);
+    this.local.updateObservable(updatedTodos);
   }
 
   checkTodo(id: number): void {
-    const todos = this.local.getFromLocalStorage(this.local.LOCALKEY);
+    const todos: Todo[] =
+      this.local.getFromLocalStorage(this.local.LOCALKEY) || [];
     const updatedTodos = todos.map((todo: Todo) => {
       if (todo.id === id) {
         todo.checked = !todo.checked;
@@ -38,7 +41,8 @@ export class TodolistService {
   }
 
   changeTitle(id: number, title: string): void {
-    const todos = this.local.getFromLocalStorage(this.local.LOCALKEY);
+    const todos: Todo[] =
+      this.local.getFromLocalStorage(this.local.LOCALKEY) || [];
     const updatedTodos = todos.map((todo: Todo) => {
       if (todo.id === id) {
         todo.title = title;
